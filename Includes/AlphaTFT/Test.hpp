a1ad30cd60b96358aa2113431fa507212c7063e8
Chris Ohk <utilForever@gmail.com>
2020-03-06 01:37:47 +0900

    feat(project-setup): Create CMake based project

---
+++ Includes/AlphaTFT/Test.hpp
@@ -0,0 +1,6 @@
+#ifndef ALPHA_TFT_TEST_HPP
+#define ALPHA_TFT_TEST_HPP
+
+int Add(int a, int b);
+
+#endif
---
+++ Sources/AlphaTFT/Test.cpp
@@ -0,0 +1,6 @@
+#include <AlphaTFT/Test.hpp>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
